dc2ccfa0799b46082a59b39021843f8c52c99363
TahaZProg <TahaZProg@yandex.ru>
2023-11-15 16:54:20 +0300

    first commit

---
+++ src/scraping_service/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Dave'
+    dict_for_view = {'date':date, 'name':name}
+    return render(request, 'home.html', dict_for_view)
